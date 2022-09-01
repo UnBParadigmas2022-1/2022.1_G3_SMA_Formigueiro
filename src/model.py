e2492c213aa44e776aa2131b8507b1c8bccb8acd
Joao-Moura <jaummoura15@gmail.com>
2022-09-01 20:47:37 -0300

    Adicionando model das colônias
    Co-authored-by: DafneM <moretti.dafne2@gmail.com>

--- src/model.py
+++ src/model.py
@@ -1,7 +1,34 @@
 import mesa
 
+from src.agents.foragingAgent import ForagingAnt
 
-class Teste(mesa.Model):
+
+class Anthill(mesa.Model):
     
-    def __init__(self):
-        self.grid = mesa.space.MultiGrid(100, 100, torus=True)
+    def __init__(self, initial_ants, initial_ants_group):
+        self.current_id = 1
+        self.width = 100
+        self.height = 100
+
+        self.schedule = mesa.time.RandomActivation(self)
+        self.grid = mesa.space.Grid(self.width, self.height, torus=False)
+
+        self.initial_ants = initial_ants
+        self.initial_ants_group = initial_ants_group
+
+        # Inicialização dos formigueiros
+        groups = []
+        for group in range(self.initial_ants_group):
+            groups.append((
+                self.random.randrange(self.width),
+                self.random.randrange(self.height)
+            ))
+
+        # Inicialização das formigas operárias
+        for ant in range(self.initial_ants):
+            pos = groups[ant % self.initial_ants_group]
+            f = ForagingAnt(self.next_id(), self, pos)
+            self.grid.place_agent(f, pos)
+            self.schedule.add(f)
+
+        self.running = True
