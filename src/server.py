5445ca1ac5c09f811c87aa22f6e792cac4421aa4
Joao-Moura <jaummoura15@gmail.com>
2022-09-01 20:47:51 -0300

    Adicionando configurações do server
    Co-authored-by: DafneM <moretti.dafne2@gmail.com>

--- src/server.py
+++ src/server.py
@@ -1,19 +1,40 @@
 import mesa
 
-from .model import Teste
+from src.model import Anthill
+from src.agents.foragingAgent import ForagingAnt
 
 
-def formigueiro(agent):
-    pass
+def ant(agent):
+    if agent is None:
+        return
+
+    portrayal = {}
+
+    if type(agent) is ForagingAnt:
+        portrayal["Color"] = ["#00FF00", "#00CC00", "#009900"]
+        portrayal["Shape"] = "rect"
+        portrayal["Filled"] = "true"
+        portrayal["Layer"] = 0
+        portrayal["w"] = 1
+        portrayal["h"] = 1
+    
+    return portrayal
 
 
-canvas_element = mesa.visualization.CanvasGrid(formigueiro, 100, 100, 600, 600)
+canvas_element = mesa.visualization.CanvasGrid(ant, 100, 100, 600, 600)
 
 
-model_params = {}
+model_params = {
+    "initial_ants": mesa.visualization.Slider(
+        "População inicial de formigas", 100, 10, 300
+    ),
+    "initial_ants_group": mesa.visualization.Slider(
+        "Quantidade inicial de formigueiros", 2, 1, 10
+    )
+}
 
 
 server = mesa.visualization.ModularServer(
-    Teste, [canvas_element], "Formigueiro", model_params
+    Anthill, [canvas_element], "Formigueiro", model_params
 )
 server.port = 8521
